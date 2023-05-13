4ddb0aee45e78bb7bc080e64ba5cb52c5a0d9c84
Deus1917 <clender.2003@mail.ru>
2023-05-13 16:26:01 +0300

    initial commit

---
+++ PZ_17/PZ_17_1.py
@@ -0,0 +1,41 @@
+# Создайте класс «Студент», который имеет атрибуты имя, фамилия и оценки.
+# Добавьте методы для вычисления среднего балла и определения, является ли студент
+# отличником.
+
+class Student:
+    def __init__(self, name, last_name, marks1, marks2, marks3 ):
+        self.name = name
+        self.last_name = last_name
+        self.marks1 = marks1
+        self.marks2 = marks2
+        self.marks3 = marks3
+
+    def gpa(self):
+        self.result = (self.marks1 + self.marks2 + self.marks3) / 3
+        self.result_text = "Средний балл у Студента", self.name, self.last_name, ':', self.result
+        self.string = ''
+        for i in self.result_text:
+            self.string += str(i)
+            self.string += ' '
+        return self.string
+
+    def the_best(self):
+        self.string_1 = ''
+        if self.result >= 4.5:
+            self.result_text_victory = "Студент ", self.name, self.last_name, "отличник!"
+            for i in self.result_text_victory:
+                self.string_1 += str(i)
+                self.string_1 += ' '
+            return self.string_1
+        else:
+            self.result_text_fail = "Студент ", self.name, self.last_name, ", к сожалению, не отличник!"
+            for i in self.result_text_fail:
+                self.string_1 += str(i)
+                self.string_1 += ' '
+            return self.string_1
+
+
+StudentOne = Student("Игорь", "Самойлов", 5, 4, 2)
+print(StudentOne.gpa())
+print(StudentOne.the_best())
+
---
+++ PZ_17/PZ_17_2.py
@@ -0,0 +1,37 @@
+# Создайте класс "Автомобиль", который содержит информацию о марке, модели и
+# годе выпуска. Создайте класс "Грузовик", который наследуется от класса
+# "Автомобиль" и содержит информацию о грузоподъемности. Создайте класс
+# "Легковой автомобиль", который наследуется от класса "Автомобиль" и содержит
+# информацию о количестве пассажиров.
+
+class Automobile:
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+
+    def get_auto(self):
+        return self.mark, self.model, self.year
+
+class Truck(Automobile):
+    def __init__(self, mark, model, year, capacity):
+        Automobile.__init__(self, mark, model, year)
+        self.capacity = capacity
+
+    def get_truck(self):
+        return self.mark, self.model, self.year, self.capacity
+
+class Car(Automobile):
+    def __init__(self, mark, model, year, capacity_people):
+        Automobile.__init__(self, mark, model, year)
+        self.capacity_people = capacity_people
+    def get_car(self):
+        return self.mark, self.model, self.year, self.capacity_people
+
+AutomobileOne = Automobile("Mercedes", "Benz C-W205", 2019)
+print(AutomobileOne.get_auto())
+AutomobileTwo = Truck("Volvo", "VNL", 2020, 3000)
+print(AutomobileTwo.get_truck())
+AutomobileThree = Car("Toyota", "Camry", 2021, 5)
+print(AutomobileThree.get_car())
+
